1066c1dac27fd75edbdb5dfab79f48a759c777a3
claudia <clou7loymed@gmail.com>
2017-04-03 00:11:09 -0500

    vnce

--- js/main.js
+++ js/main.js
@@ -34,14 +34,40 @@ $(function(){
   });
 });
 
+
 function onClickAboutMe() {
 	gotoSection('aboutMe');
-    $('#aboutMe').click(function(){
-    var texto = $('#text-animation');
-    texto.animate({right:'700px', opacity:'0.4'}, "slow");
-});
-    
+        
 }
+
+document.addEventListener('DOMContentLoaded',function(event){
+  var dataText = [ "CLAUDIA MARIEL LOAYZA MEDINA", "CLAUDIA MARIEL LOAYZA MEDINA"];
+  
+  function typeWriter(text, i, fnCallback) {
+    if (i < (text.length)) {
+     document.querySelector("h1").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    else if (typeof fnCallback == 'function') {
+      setTimeout(fnCallback, 700);
+    }
+  }
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+    if (i < dataText[i].length) {
+     typeWriter(dataText[i], 0, function(){
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  StartTextAnimation(0);
+});
 
 
 
